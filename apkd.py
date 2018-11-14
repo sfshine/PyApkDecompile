eea1c6b0a9643225f2d3df13c3163aa6c9f5619c
gaoshuai <gaoshuai@teddymobile.cn>
2018-11-14 18:52:53 +0800

    add open jar

--- apkd.py
+++ apkd.py
@@ -3,29 +3,32 @@ import sys
 import os
 import zipfile
 #定义反编译路径, 把dex2jar, ji-gui等放到ApkDecompile文件夹下
-dapk_path=os.path.abspath('apkd.py')
-decompile_lib=dapk_path[0:dapk_path.rindex("/")] + "/libs/"
+dfile_path=os.path.abspath('apkd.py')
+decompile_lib=dfile_path[0:dfile_path.rindex("/")] + "/libs/"
 
 if len(sys.argv) <2:
 	print('Usage: python apkd.py [Apk/Dex filepath]')
 else:
-	apk_path = sys.argv[1]
-	apk_decompile_path = apk_path[0:apk_path.index(".")]
+	file_path = sys.argv[1]
+	apk_decompile_path = file_path[0:file_path.index(".")]
 
-	print("apkd will process:"+apk_path)
+	print("apkd will process:"+file_path)
 	#反编译apk
-	if "apk" in apk_path:
-		print("process apk:" + apk_path)
-		os.system("{}/apktool.sh d {} -o {} -f".format(decompile_lib, apk_path, apk_decompile_path))
-		print (apk_path[0:apk_path.index(".")])
-		filesInZip = zipfile.ZipFile(apk_path, "r")
+	if "apk" in file_path:
+		print("process apk:" + file_path)
+		os.system("{}/apktool.sh d {} -o {} -f".format(decompile_lib, file_path, apk_decompile_path))
+		print (file_path[0:file_path.index(".")])
+		filesInZip = zipfile.ZipFile(file_path, "r")
 		filesInZip.extract("classes.dex", apk_decompile_path)
 		filesInZip.close()
-		apk_path = apk_decompile_path + "/classes.dex"
+		file_path = apk_decompile_path + "/classes.dex"
 
 	#反编译dex
-	if "dex" in apk_path:
-		print("process dex:"+apk_path)
-		os.system("{}/dex2jar-2.0/d2j-dex2jar.sh {} -o {}/output.jar --force".format(decompile_lib, apk_path ,apk_decompile_path))
-
-	os.system("java -jar {}/jd-gui/jd-gui-1.4.0.jar {}/output.jar".format(decompile_lib, apk_decompile_path))
+	if "dex" in file_path:
+		print("process dex:"+file_path)
+		jar_path = "{}/output.jar".format(apk_decompile_path)
+		os.system("{}/dex2jar-2.0/d2j-dex2jar.sh {} -o {} --force".format(decompile_lib, file_path ,jar_path))
+	#直接打开jar
+	if "jar" in file_path:
+		jar_path = file_path;
+	os.system("java -jar {}/jd-gui/jd-gui-1.4.0.jar {}".format(decompile_lib, jar_path))
